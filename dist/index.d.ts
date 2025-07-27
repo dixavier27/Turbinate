7361889d3589f6f036839f6c4ba0d3c39355e0fc
Affonso Xavier <dixavier27@gmail.com>
2025-07-27 12:05:30 -0400

    refactor: Remover métodos não utilizados e ajustar a interface iTurbinate

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-import { RootFilterQuery, UpdateQuery, Types, Document, Model, AnyKeys } from 'mongoose';
+import { RootFilterQuery, UpdateQuery, Types, Document, Model } from 'mongoose';
 
 interface iTurbinate<T> {
     create(data: T): Promise<T>;
@@ -12,12 +12,6 @@ declare class Turbinate<T> implements iTurbinate<T> {
     protected model: Model<T>;
     protected parentModel?: Model<T> | undefined;
     constructor(model: Model<T>, parentModel?: Model<T> | undefined);
-    create(data: T): Promise<T>;
-    update(filter: RootFilterQuery<T>, data: UpdateQuery<T>, unset?: AnyKeys<T>): Promise<T | null>;
-    replace(_id: Types.ObjectId, data: T): Promise<T | null>;
-    read(filters: RootFilterQuery<T>): Promise<Document<T> | null>;
-    search(filters: RootFilterQuery<T>): Promise<T[]>;
-    remove(_id: Types.ObjectId): Promise<T | null>;
 }
 
-export { Turbinate as default, type iTurbinate };
+export { Turbinate as default };
